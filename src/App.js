a857c584943a3b9fd6bc0075a069cfcadfa8442e
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-05-15 14:02:17 -0300

    versão com datatest4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Jogo from "./Jogo";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-//import reportWebVitals from './reportWebVitals';
+
 
 
 export default function App(){
     
+
     return(
         <div class="containerJogo">
         <Jogo />
@@ -16,9 +15,4 @@ export default function App(){
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
+
